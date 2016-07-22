e558c42fc9069f3ffc41eda83511ce268d3618d2
Darko Stankovski <darko.itpro@gmail.com>
2016-07-22 09:02:54 +0200

    Aides on unit test example

---
+++ tests/poo/ordered_tasks_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from training.poo.tasks import Task
+
+
+class TestCompareDueDate(unittest.TestCase):
+
+    def testBasic(self):
+        from datetime import datetime
+        t1 = Task("task1", datetime(2015, 01, 01), datetime(2016, 01, 01))
+        t2 = Task("task2", datetime(2015, 01, 01), datetime(2016, 01, 02))
+        self.assertGreater(t2, t1)
+
+    def testNone(self):
+        from datetime import datetime
+        t1 = Task("task1", datetime(2015, 01, 01))
+        t2 = Task("task2", datetime(2015, 01, 01), datetime(2016, 01, 02))
+        self.assertGreater(t1, t2)
+
+    def testSameDueDifferentCreation(self):
+        from datetime import datetime
+        t1 = Task("task1", datetime(2015, 01, 02), datetime(2016, 01, 01))
+        t2 = Task("task2", datetime(2015, 01, 01), datetime(2016, 01, 01))
+        self.assertGreater(t1, t2)
