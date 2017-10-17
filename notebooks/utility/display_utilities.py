2a88f5ac0515c706231c37b8474415a9f6248de7
Darko Stankovski <darko.itpro@gmail.com>
2017-10-17 12:38:14 +0200

    Ajout notebook dépendances

---
+++ notebooks/utility/display_utilities.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+
+def display_elements(collection):
+    """
+    Affiche chaque élément d'une collection
+    :param collection: la collection à afficher
+    """
+    for element in collection:
+        print(element)
