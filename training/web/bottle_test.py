269f3827bd363020fe4635882d9de86d8caba58d
Darko Stankovski <darko.itpro@gmail.com>
2017-11-26 23:08:45 +0100

    Ajout des bases des card games, basic stack et serveur web json

--- training/web/bottle_test.py
+++ training/web/bottle_test.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*-
+
+import json
+
+from bottle import run, get, post, response
+
+@get('/names')
+def get_listing_names_handler():
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Cache-Control'] = 'no-cache'
+    return json.dumps({'names': ['Arthur', 'Lancelot', 'Galahad']})
+
+if __name__ == '__main__':
+    run(host='127.0.0.1', port=8000)
