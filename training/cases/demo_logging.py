920b6c7d40b0d287b2bfc1a41f69d3c8953fcec7
Darko Stankovski <darko.itpro@gmail.com>
2018-06-24 11:02:22 +0200

    Ajout de démos regex et logger

---
+++ training/cases/demo_logging.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+import logging
+
+logging.basicConfig(filename="file.log", level=logging.INFO)
+logging.warning("Un warning")
+logging.info("Une info")
