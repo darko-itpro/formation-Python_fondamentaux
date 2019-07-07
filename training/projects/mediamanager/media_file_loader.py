8bd9825be29d4dbb5eaf1bc820c940b1c8bd7374
Darko Stankovski <darko.itpro@gmail.com>
2019-07-07 23:23:01 +0200

    Ajout fonction parsing fichiers media CSV Ajout main pour musées listant données

--- training/projects/mediamanager/media_file_loader.py
+++ training/projects/mediamanager/media_file_loader.py
@@ -11,8 +11,6 @@ import os
 
 SERIES_RE = re.compile("-s([0-9]{2})e([0-9]{2})-")
 
-SERIES_FULL_NAME = "Silicon_Valley-s01e03-Articles_Of_Incorporation.avi"
-
 
 def get_series_value_from_file(filename):
     """
@@ -21,11 +19,12 @@ def get_series_value_from_file(filename):
     * Numéro de la saison
     * Numéro de l'épisode
     * Titre de l'épisode
-    * Extension du fichier
+    * None (durée)
+    * None (année)
 
     :param filename: nom du fichier duquel extraire les données
     :return: une liste de chaines de caractères, voir description.
-    :return type: list
+    :return type: tuple
     """
     series_file_name, series_ext = os.path.splitext(filename)
 
@@ -36,7 +35,27 @@ def get_series_value_from_file(filename):
                series_match.group(1),\
                series_match.group(2),\
                series_file_name[series_match.end():].replace('_', ' '),\
-               series_ext
+               None,\
+               None
+
+
+def get_series_values_from_csv(record):
+    """
+    Retourne les données sur un épisode. Les valeurs sont :
+    * Nom de la série
+    * Numéro de la saison
+    * Numéro de l'épisode
+    * Titre de l'épisode
+    * Durée
+    * Année
+
+
+    :param record: nom du fichier duquel extraire les données
+    :return: une liste de chaines de caractères, voir description.
+    :return type: tuple
+    """
+    data = record.split(";")
+    return tuple(data)
 
 
 def load_episode_from_file(filepath):
@@ -48,8 +67,16 @@ def load_episode_from_file(filepath):
     :return: Une liste de tuples (titre série, numéro série, numéro épisode, titre épisode)
     :raise OSError: if file does not exist.
     """
+
     with open(filepath, 'r') as media_file:
-        media_data = [get_series_value_from_file(media[:-1])
+        if os.path.splitext(filepath)[1] == ".csv":
+            media_file.readline()
+            get_series_values = get_series_values_from_csv
+
+        else:
+            get_series_values = get_series_value_from_file
+
+        media_data = [get_series_values(media[:-1])
                       for media in media_file]
 
     return media_data
@@ -58,6 +85,8 @@ def load_episode_from_file(filepath):
 if __name__ == '__main__':
 
     import argparse
+
+    SERIES_FULL_NAME = "Silicon_Valley-s01e03-Articles_Of_Incorporation.avi"
 
     PARSER = argparse.ArgumentParser(description='')
     PARSER.add_argument("-f", "--file", help="Path to the file to parse",
