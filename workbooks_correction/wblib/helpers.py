b8e5917c6348a7667188eb840d53660b04142f0f
Darko Stankovski <darko.itpro@gmail.com>
2019-09-29 20:35:52 +0200

    Mise à jour des callepins

---
+++ workbooks_correction/wblib/helpers.py
@@ -0,0 +1,48 @@
+"""
+Ceci est un module destiné à recevoir le code des fonctions mutualisées au sein
+des notebooks. Vous pouvez y ajouter les fonction set les ressources que vous
+pensez utiles.
+
+Notez que dans un projet *normal* un module de ce type ne devrait être dédié
+qu'à une seule unité fonctionnelle. Dans le cadre de la formation, nous allons
+nous permettre de transgresser ce principe. Mais vous pouvez évidemment créer
+d'autres modules (fichiers texte avec l'extension .py).
+
+Notez également que ce commentaire est une documentation du module. Il s'agit
+d'une docstring du module.
+"""
+
+
+def duration_for(how_many: int, unit_duration=7):
+    """
+    Calcul une durée totale en fonction d'une unité de temps.
+
+    :param how_many: Le nombre d'unités de temps. Doit être positif.
+    :param unit_duration: La durée d'une unité de temps, par défaut de 7 qui
+    correspond à la durée d'une journée de travail. Doit être positif.
+    :return: La durée totale.
+    """
+    how_many = int(how_many)
+    unit_duration = int(unit_duration)
+
+    if how_many < 0 or unit_duration < 0:
+        raise ValueError("Les paramètres doivent être positifs.")
+
+    return int(how_many) * int(unit_duration)
+
+
+def to_minutes(hours: int, minutes=0):
+    """
+    calcul une durée en minutes en fonction d'heures et minutes.
+
+    :param hours: une durée en heure, doit être positif.
+    :param minutes: Une durée optionnelle en minutes. 0 par défaut. Doit être
+    positif.
+    :return: La durée totale en minutes.
+    """
+    hours = int(hours)
+    minutes = int(minutes)
+    if hours < 0 or minutes < 0:
+        raise ValueError("Les durées doivent être positives")
+
+    return int(hours) * 60 + int(minutes)
