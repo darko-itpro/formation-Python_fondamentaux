4b394345c6f4eb99e47d9afdbc22f4b8e458c2ad
Darko Stankovski <darko.itpro@gmail.com>
2020-02-22 04:16:34 +0100

    Mise à jour tests et ajout utils affichage

--- draft/media/display_utils.py
+++ draft/media/display_utils.py
@@ -8,9 +8,27 @@ def display_episodes(show):
     """
     print(f"{show.name}")
     if show.episodes:
-        for episode in show:
+        for episode in show.episodes:
             if hasattr(episode, "title"):
-                print(f" - {episode.title} e{episode.number}s{episode.season.number}")
+                print(f" - {episode.title} e{episode.number}s{episode.season_number}")
+            else:
+                print(f" - {episode}")
+    else:
+        print(" - Pas d'épisodes pour cette série - ")
+
+
+def display_episodes_for_season(show, season:int = None):
+    """
+    Display episodes for a given show and a given season number
+
+    :param show: une série sous forme d'objet
+    :param season: le numéro de saison ou None.
+    """
+    print(f"{show.name}")
+    if show.get_episodes(season):
+        for episode in show.get_episodes(season):
+            if hasattr(episode, "title"):
+                print(f" - {episode.title} e{episode.number}s{episode.season_number}")
             else:
                 print(f" - {episode}")
     else:
