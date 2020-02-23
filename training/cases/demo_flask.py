a8ebe4ba20aedfb603ff098d963fc91b39c0af08
Darko Stankovski <darko.itpro@gmail.com>
2020-02-24 00:21:07 +0100

    Ajout Flask URL variable

--- training/cases/demo_flask.py
+++ training/cases/demo_flask.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Hello world !"
+
+
+@app.route('/<demo_var>/')
+def var(demo_var):
+    return f"url : {demo_var}"
+
 
 if __name__ == "__main__":
     app.run()
