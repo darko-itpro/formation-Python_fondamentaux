f9388d0338ef699160f0a45a346daee55701f083
Darko Stankovski <darko.itpro@gmail.com>
2020-03-12 12:24:43 +0100

    Mise à jour projet Flask

--- training/projects/mediamanager/mediaweb.py
+++ training/projects/mediamanager/mediaweb.py
@@ -16,44 +16,45 @@ def index():
     return render_template('media_home.html')
 
 
-@app.route('/shows/')
+@app.route('/shows/', methods=['GET', 'POST'])
 def index_show():
-    _MY_SHOWS = media_db.MediaDao()
-    _MY_SHOWS.get_shows()
-    return render_template('series.html', series=_MY_SHOWS.get_shows())
+    if request.method == "POST":
+        show_title = request.form['new_show']
+        media_db.TvShow(show_title)
+        return redirect("/shows/")
+
+    else:
+        _MY_SHOWS = media_db.MediaDao()
+        _MY_SHOWS.get_shows()
+        return render_template('series.html', series=_MY_SHOWS.get_shows())
 
 
-@app.route('/add/', methods=['POST'])
-def add_show():
-    show_title = request.form['new_show']
-    my_new_show = media_db.TvShow(show_title)
-    del my_new_show
-    return redirect("/shows/")
-
-
-@app.route('/show/<name>')
-def view_show_details(name):
-    name = name.replace('_', ' ')
-    selected_show = media_db.TvShow(name)
+@app.route('/show/<show_name>')
+def view_show_details(show_name):
+    show_name = show_name.replace('_', ' ')
+    selected_show = media_db.TvShow(show_name)
     return render_template('show_detail.html', title=selected_show.name,
                            episodes=selected_show.get_episodes())
 
+@app.route('/show/<show_name>/new_episode/')
+def add_episode(show_name):
+    return render_template('show_add.html', title=show_name)
 
-@app.route('/show/<name>/add')
-def add_show_details(name):
-    name = name.replace('_', ' ')
-    selected_show = media_db.TvShow(name)
-    return render_template('show_add.html', title=selected_show.name)
+@app.route('/show/<show_name>/episodes/', methods=['GET', 'POST'])
+def episode(show_name):
+    show_name = show_name.replace('_', ' ')
 
-
-@app.route('/add/<name>/episode/add', methods=['POST'])
-def view_add_episode(name):
-    episode_title = request.form['episode_title']
-    episode_number = request.form['episode_number']
-    episode_season = request.form['season_number']
-    selected_show = media_db.TvShow(name)
-    selected_show.add_episode(episode_title, episode_number, episode_season)
-    return redirect("/show/%s" % name)
+    if request.method == 'POST':
+        episode_title = request.form['episode_title']
+        episode_number = request.form['episode_number']
+        episode_season = request.form['season_number']
+        selected_show = media_db.TvShow(show_name)
+        selected_show.add_episode(episode_title, episode_number, episode_season)
+        return redirect(f"/show/{show_name}")
+    else:
+        selected_show = media_db.TvShow(show_name)
+        return render_template('show_detail.html', title=selected_show.name,
+                               episodes=selected_show.get_episodes())
 
 
 if __name__ == '__main__':
