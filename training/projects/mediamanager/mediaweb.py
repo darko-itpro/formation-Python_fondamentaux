b44df64c72f2b5e0ba0a3cd34ecee2aec5e53144
Darko Stankovski <darko.itpro@gmail.com>
2020-03-12 16:54:29 +0100

    Ajout page 404 projet flask mediamanger

--- training/projects/mediamanager/mediaweb.py
+++ training/projects/mediamanager/mediaweb.py
@@ -57,5 +57,9 @@ def episode(show_name):
                                episodes=selected_show.get_episodes())
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("page_not_found.html"), 404
+
 if __name__ == '__main__':
     app.run()
