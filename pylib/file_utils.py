9e4bbc19fa45e1d42f1b3ff1799f6071518ce03f
Darko Stankovski <darko.itpro@gmail.com>
2023-01-30 14:14:09 +0100

    Ajout fichier accès données

---
+++ pylib/file_utils.py
@@ -0,0 +1,27 @@
+def load_from_filenames(path):
+    import re
+    import os
+    import os.path
+
+    pattern = r"-s(?P<season>[0-9]?)e(?P<episode>[0-9]2)-"
+
+    for item in os.scandir(path):
+        file_name = os.path.splitext(item.name)[0]
+        found = re.search(pattern, file_name)
+        if found:
+            number = int(found.group("episode"))
+            season = int(found.group("season"))
+            show = file_name[:found.start()].replace("_", " ")
+            title = file_name[:found.start()].replace("_", " ")
+
+            yield show, season, number, title
+
+
+def load_from_csv(path):
+    with open(path) as episodes_file:
+        episodes_file.readline()
+
+        for episode in episodes_file:
+            yield tuple(episode.split(";"))
+
+    return None
