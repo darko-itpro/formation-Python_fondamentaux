a73e4c5ef066941eebc425ed9800bac022ed951d
dad3zero <dad3zero@gmail.com>
2023-09-20 19:15:08 +0200

    Ajout fichier pour démos

---
+++ demos/demo_data.py
@@ -0,0 +1,9 @@
+camelot = ["Arthur", "Merlin", "Lancelot", "Robin", "Perceval", "Karadoc"]
+wizards = ["Merlin", "Tim"]
+
+camelot = [("Arthur", "king"), ("Merlin", "wizard"),
+           ("Lancelot", "knight"), ("Robin", "knight"),
+           ("Perceval", "knight"), ("Karadoc", "knight")]
+
+camelot = {"king":"Arthur", "wizard":"Merlin",
+           "knights":["Lancelot", "Robin", "Perceval", "Karadoc"]}
