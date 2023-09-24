4ea7291a8ff9fdc3ffbec4c90dba73dc32d86f21
dad3zero <dad3zero@gmail.com>
2023-09-24 22:51:59 +0200

    Ajout démos PySide

---
+++ demos/pyside_examples/qtdemo_01_label.py
@@ -0,0 +1,18 @@
+import sys
+from PySide6 import QtCore, QtWidgets, QtGui
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.label = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.label)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.show()
+
+    sys.exit(app.exec())
---
+++ demos/pyside_examples/qtdemo_02_button_slots.py
@@ -0,0 +1,31 @@
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self._hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self._label = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
+        self._button = QtWidgets.QPushButton('Click Me')
+
+        self._layout = QtWidgets.QVBoxLayout(self)
+        self._layout.addWidget(self._label)
+        self._layout.addWidget(self._button)
+
+        self._button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self._label.setText(random.choice(self._hello))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    #widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
