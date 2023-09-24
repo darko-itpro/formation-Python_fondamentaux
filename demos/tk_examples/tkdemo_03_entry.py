804cfa0328adfb0d4cd30297ac735e5803034c1f
dad3zero <dad3zero@gmail.com>
2023-09-24 22:19:11 +0200

    Renommage des sources Tkinter

--- demos/tk_examples/tkdemo_03_entry.py
+++ demos/tk_examples/tkdemo_03_entry.py
@@ -7,9 +7,6 @@ import tkinter as tk
 window = tk.Tk()
 window.title('Demo entry')
 
-value = tk.StringVar()
-tk.Entry(window, textvariable=value, width=30).pack()
-
 
 def set_value():
     value.set("Default value")
@@ -18,6 +15,9 @@ def set_value():
 def log():
     print(f"Log value {value.get()}")
 
+
+value = tk.StringVar()
+tk.Entry(window, textvariable=value, width=30).pack()
 
 tk.Button(window, text="Set Default", command=set_value).pack()
 tk.Button(window, text="Log", command=log).pack()
--- demos/tk_examples/tkdemo_04_check.py
+++ demos/tk_examples/tkdemo_04_check.py
@@ -7,15 +7,16 @@ import tkinter as tk
 window = tk.Tk()
 window.title('Demo entry')
 
+
+def log():
+    print(f"Log value {value.get() * (1 + vat.get() / 100)}")
+
+
 value = tk.IntVar()
 vat = tk.IntVar()
 
 tk.Entry(window, textvariable=value, width=30).pack()
 tk.Checkbutton(window, text="TTC", variable=vat, onvalue=0, offvalue=20).pack()
-
-def log():
-    print(f"Log value {value.get() * (1 + vat.get() / 100)}")
-
 
 tk.Button(window, text="Log", command=log).pack()
 tk.Button(window, text="Quit", command=window.quit).pack()
