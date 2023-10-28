30db476cb63ad113d6fde077f01f3d8b21bf4228
dad3zero <dad3zero@gmail.com>
2023-10-28 14:13:52 +0200

    Correction fonctions de chargement de données

--- pylib/utils/file_helper.py
+++ pylib/utils/file_helper.py
@@ -5,7 +5,7 @@ import re
 def load_from_filenames(dir_path):
     """
     Générateur qui fournit les informations média série à partir du nom des fichiers du répertoire.
-    
+
     :param dir_path: Chemin vers un répertoire de fichiers média correctement formatés
     """
     pattern = "-s(?P<season>[0-9]{2})e(?P<episode>[0-9]{2})-"
@@ -16,16 +16,16 @@ def load_from_filenames(dir_path):
 
         if result:
             yield episode_name[:result.start()].replace("_", " "), \
-                  result.group("season"), result.group("episode"), \
-                  episode_name[result.end():].replace("_", " ")
+                episode_name[result.end():].replace("_", " "), \
+                result.group("episode"), result.group("season")
 
 
 def load_from_csv(file_path):
     """
     Générateur qui fournit les informations média série à partir d'un fichier csv
-    
+
     :param file_path: Chemin vers un fichier csv correctement formaté
-    :return: 
+    :return:
     """
     with open(file_path, encoding="utf-8") as csv_file:
         header = tuple(csv_file.readline().strip().split(";"))
@@ -33,4 +33,5 @@ def load_from_csv(file_path):
             raise ValueError("Unexpected CSV structure")
 
         for episode in csv_file:
-            yield tuple(episode.strip().split(";"))
+            show, season, number, title, duration, year = episode.strip().split(";")
+            yield show, title, number, season, duration, year
