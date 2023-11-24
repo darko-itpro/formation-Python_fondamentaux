5a6b02c64baf200bf80adcefb6ed3ddc8e92e07c
dad3zero <dad3zero@gmail.com>
2023-11-24 21:39:32 +0100

    Modification paramètres Episode

--- stage/pyflix/mediatheque.py
+++ stage/pyflix/mediatheque.py
@@ -1,7 +1,7 @@
 
 class Episode:
     def __init__(self, title: str, number: int, season_number: int,
-                 duration: int, year: int):
+                 duration: int = None, year: int = None):
         self.title = title
         self.number = number
         self.season_number = season_number
