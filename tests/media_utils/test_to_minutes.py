8a78a13ad4efe243acd3aca72c3db0f5aba6f4cd
Darko Stankovski <darko.itpro@gmaiL.com>
2024-07-02 11:11:44 +0200

    Ajout des tests

---
+++ tests/media_utils/test_to_minutes.py
@@ -0,0 +1,16 @@
+from exos_correction.media_utils import to_minutes
+
+def test_to_minutes_with_hours():
+    assert to_minutes(2) == 120
+
+def test_to_monutes_0_hour():
+    assert to_minutes(0) == 0
+
+def test_to_minutes_with_hours_as_str():
+    assert to_minutes('2') == 120
+
+def test_to_minutes_with_hours_and_minutes():
+    assert to_minutes(2, 14) == 134
+
+def test_to_minutes_with_hours_and_minutes_as_str():
+    assert to_minutes('2', '14') == 134
