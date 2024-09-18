84a8d957dd45f70a9bc87c792ae5eabda9c7aa12
Darko Stankovski <darko.itpro@gmaiL.com>
2024-09-18 20:46:28 +0200

    Exercice listes, ajout manquant

---
+++ exos_corrections/exo_lists.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+import pyflix.datasource as ds
+
+
+def season_duration(episodes:list, episode_duration:int):
+    return len(episodes) * episode_duration
+
+def get_playlist(season:list, episode_duration:int, duration:int):
+    n = duration // episode_duration
+    return season[:n]
+
+if __name__ == "__main__":
+    bbt_s12 = ds.get_season()
+    EPISODE_DURATION = 23
+
+    print(season_duration(bbt_s12, EPISODE_DURATION))
+
+    remaining_duration = 120
+
+    pprint(get_playlist(bbt_s12, EPISODE_DURATION, remaining_duration))
