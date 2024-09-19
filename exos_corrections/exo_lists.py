88057e12203d4fd18129df725090f9bff13b68b9
Darko Stankovski <darko.itpro@gmaiL.com>
2024-09-19 10:25:47 +0200

    Correction définitive listes

--- exos_corrections/exo_lists.py
+++ exos_corrections/exo_lists.py
@@ -18,3 +18,14 @@ if __name__ == "__main__":
     remaining_duration = 120
 
     pprint(get_playlist(bbt_s12, EPISODE_DURATION, remaining_duration))
+
+    print("\n-- La playlist du soir --")
+
+    playlist = bbt_s12.copy()
+    print(playlist.pop(0))
+
+
+    print(season_duration(playlist, EPISODE_DURATION))
+    print(season_duration(bbt_s12, EPISODE_DURATION))
+
+    print(playlist is bbt_s12)
