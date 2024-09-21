7b2c7dd88eec2ee15bd9c5b2a02865111b93d518
Darko Stankovski <darko.itpro@gmaiL.com>
2024-09-21 11:29:09 +0200

    Réorganisation de la structure du projet

--- exos/exo_04.py
+++
@@ -1,6 +0,0 @@
-
-episode_not_viewed_1 = ["The new Project", 1, 98, False]
-episode_not_viewed_2 = ['Installing the softwares', 2, 42]
-
-episode = episode_not_viewed_1
-# episode = episode_not_viewed_2
