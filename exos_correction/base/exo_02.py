634d01045d10140ea1a4d9d8463d4c1cb0b98ae9
Darko Stankovski <darko.itpro@gmaiL.com>
2024-09-30 15:01:47 +0200

    Après second exercice

--- exos_correction/base/exo_02.py
+++ exos_correction/base/exo_02.py
@@ -1,2 +1,12 @@
 
 episode = ["The new Project", 1, 98, 2]
+
+title, _, duration, viewed = episode
+
+print(title, duration)
+
+print(episode)
+
+episode[3] = episode[3] + 1
+episode[3] += 1
+print(episode)
