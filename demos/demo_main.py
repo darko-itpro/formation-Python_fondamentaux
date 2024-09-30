a99648dd56ee96cd68e1f75fc3f64838633f5c94
Darko Stankovski <darko.itpro@gmaiL.com>
2024-09-30 17:04:15 +0200

    Exemples import

---
+++ demos/demo_main.py
@@ -0,0 +1,8 @@
+#import demos.myhome
+#print(demos.myhome.confort(30))
+
+from demos import myhome
+print(myhome.confort(20))
+
+#import demos.myhome as mh
+#print(mh.confort(10))
--- demos/myhome.py
+++ demos/myhome.py
@@ -2,11 +2,13 @@
 Module de contrôle du confort à la maison… Ou plutôt, qui va le simuler.
 """
 
-def confort(temperature:int):
-    if temperature > 26:
-        return "Chaud"
-    elif temperature < 18:
-        return "froid"
-    else:
-        return "bon"
+def confort(temperature:int|str) -> str:
+    temperature = int(temperature)
 
+    value = "bon"
+    if temperature > 26:
+        value = "Chaud"
+    elif temperature < 18:
+        value = "froid"
+
+    return value
