b9cab084d3220d3dcb739b2c3b48b5a9231b1f12
Darko Stankovski <darko.itpro@gmaiL.com>
2024-09-30 15:44:19 +0200

    Après fonctions

--- exos_correction/base/exo_03_02.py
+++ exos_correction/base/exo_03_02.py
@@ -3,4 +3,11 @@ episode_viewed = ["The new Project", 1, 98, 2]
 episode_not_viewed = ['Installing the softwares', 2, 42, 0]
 
 episode = episode_viewed
-# episode = episode_not_viewed
+episode = episode_not_viewed
+
+viewed = episode[3]
+
+
+def is_viewed(episode):
+    viewed = episode[3]
+    return bool(viewed)
