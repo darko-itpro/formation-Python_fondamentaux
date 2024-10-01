606ef5ed9fb6c5176f31925fc6990375f1b52e43
Darko Stankovski <darko.itpro@gmaiL.com>
2024-10-01 11:50:03 +0200

    Test paramétré

--- tests_correction/exos/test_is_viewed.py
+++ tests_correction/exos/test_is_viewed.py
@@ -1,21 +1,13 @@
+import pytest
 from exos_correction.base.episodes_utils import is_viewed
 
-def test_episode_viewed_as_bool():
-    episode = ["The new Project", 1, 98, True]
-    assert is_viewed(episode) is True
+test_data = [(["The new Project", 1, 98, True], True),
+             (['Installing the softwares', 2, 42, False], False),
+             (["The new Project", 1, 98, 2], True),
+             (['Installing the softwares', 2, 42, 0], False),
+             (['Installing the softwares', 2, 42], False)
+             ]
 
-def test_episode_not_viewed_as_bool():
-    episode = ['Installing the softwares', 2, 42, False]
-    assert is_viewed(episode) is False
-
-def test_episode_viewed_as_int():
-    episode = ["The new Project", 1, 98, 2]
-    assert is_viewed(episode) is True
-
-def test_episode_not_viewed_as_int():
-    episode = ['Installing the softwares', 2, 42, 0]
-    assert is_viewed(episode) is False
-
-def test_episode_not_viewed_without_viewed():
-    episode = ['Installing the softwares', 2, 42]
-    assert is_viewed(episode) is False
+@pytest.mark.parametrize("episode, expected", test_data)
+def test_episode_not_viewed_without_viewed(episode, expected):
+    assert is_viewed(episode) is expected
