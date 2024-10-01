c900ab5eabed275f35ea1be0937625f243c33963
Darko Stankovski <darko.itpro@gmaiL.com>
2024-10-01 09:59:36 +0200

    Ajout tests limite

--- tests_correction/demos/test_confort.py
+++ tests_correction/demos/test_confort.py
@@ -6,8 +6,16 @@ def test_hot_temp():
     assert confort(30) == "chaud"
 
 def test_cold_temp():
-    assert confort(10) == "foid"
+    assert confort(10) == "froid"
 
 def test_warm_temp():
     assert confort(20) == "bon"
 
+def test_warm_temp_as_str():
+    assert confort('20') == "bon"
+
+def test_limit_hot():
+    assert confort(26) == "bon"
+
+def test_limit_cold():
+    assert confort(18) == "bon"
