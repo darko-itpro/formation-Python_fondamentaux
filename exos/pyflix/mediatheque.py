c95524ed4aaa2e01e8c026124d20d9a85b195e6a
Darko Stankovski <darko.itpro@gmaiL.com>
2024-11-15 18:29:51 +0100

    Fin objet

--- exos/pyflix/mediatheque.py
+++ exos/pyflix/mediatheque.py
@@ -17,16 +17,33 @@ class Episode:
 class TvShow:
     def __init__(self, name):
         self.name = name
-        self.episodes = []
+        self._episodes = []
+
+    @property
+    def duration(self):
+        return sum([episode.duration
+                    for episode in self._episodes])
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name.title()
+
+    @property
+    def episodes(self):
+        return self._episodes.copy()
 
     def add_episode(self, title, number, season_number, duration=None, year=None):
         episode = Episode(title, number, season_number, duration, year)
-        if episode in self.episodes:
+        if episode in self._episodes:
             raise ValueError('Duplicate episode')
-        self.episodes.append(episode)
+        self._episodes.append(episode)
 
     def __str__(self):
-        return f"Show {self.name} ({len(self.episodes)} episodes)"
+        return f"Show {self.name} ({len(self._episodes)} episodes)"
 
     def __repr__(self):
         return f"TvShow({self.name})"
