fad6b2cf3053957e8aee70cfbc1db68b60ae1838
Darko Stankovski <darko.itpro@gmaiL.com>
2024-12-18 12:45:48 +0100

    Correction paramètres optionnels

--- exos/base/media_utils.py
+++ exos/base/media_utils.py
@@ -1,2 +1,21 @@
 def is_viewed(episode:dict) -> bool:
     return "viewed" in episode and bool(episode["viewed"])
+
+def add_to_playlist(episode:dict, playlist:list[dict]=None):
+    if playlist is None:
+        playlist = []
+    else:
+        playlist = playlist.copy()
+
+    playlist.append(episode)
+    return playlist
+
+
+if __name__ == "__main__":
+    pl1 = add_to_playlist({'title':'toto'})
+
+    my_plylist = [{"title": "The Conjugal Configuration", "duration": 20, "viewed": True},
+                  {"title": "The Conjugal Configuration", "duration": 20, "viewed": 3}]
+
+    my_plylist = add_to_playlist({"title": "The other Conjugal Configuration", "duration": 20, "viewed": True},
+                                 my_plylist)
