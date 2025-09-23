a539b0a9fc9545d4ffd64fbf39b99f2dad08d0b8
Darko Stankovski <darko.itpro@gmail.com>
2025-09-23 11:19:18 +0200

    Création de la fonction (exercice fonction)

--- exos/base/exo_03_01.py
+++ exos/base/exo_03_01.py
@@ -2,12 +2,9 @@
 episode_viewed = ["The new Project", 1, 98, True]
 episode_not_viewed = ['Installing the softwares', 2, 42, False]
 
-episode = episode_viewed
-# episode = episode_not_viewed
+def is_viewed(episode:list) -> bool:
+    return episode[3]
 
-viewed = episode[3]
 
-if viewed:
-    print("Episode viewed")
-else:
-    print("Episode not viewed")
+is_viewed(episode_viewed)
+is_viewed(episode_not_viewed)
