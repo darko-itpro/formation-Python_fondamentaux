7207e73c1799aab338a00033757666e35264a924
Darko Stankovski <darko.itpro@gmail.com>
2025-09-26 10:52:29 +0200

    Ajout properties

--- exos/pyflix/mediatheque.py
+++ exos/pyflix/mediatheque.py
@@ -15,7 +15,28 @@ class Episode:
 class TvShow:
     def __init__(self, name: str):
         self.name = name
-        self.episodes = []
+        self._episodes = []
+
+    @property
+    def duration(self):
+        return sum([episode.duration
+                    for episode in self._episodes])
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name: str):
+        self._name = new_name.title()
+
+    @property
+    def episodes(self):
+        return self._episodes.copy()
+
+    @episodes.deleter
+    def episodes(self):
+        self._episodes.clear()
 
     def add_episode(self, title: str,
                     season_number: int,
@@ -23,6 +44,6 @@ class TvShow:
                     duration: int = None,
                     year: int = None):
         new_episode = Episode(title, season_number, number, duration, year)
-        if new_episode in self.episodes:
+        if new_episode in self._episodes:
             raise ValueError(f"Episode '{new_episode.title}' already exists")
-        self.episodes.append(new_episode)
+        self._episodes.append(new_episode)
