6c82616575029b5eb42e1c6520c7f6dd2f0be34f
Darko Stankovski <darko.itpro@gmail.com>
2025-09-26 18:44:16 +0200

    Adaptation test lié properties

--- tests/exos/pyflix/tvshow/test_create_tvshow.py
+++ tests/exos/pyflix/tvshow/test_create_tvshow.py
@@ -3,8 +3,8 @@ import pytest
 from exos.pyflix.mediatheque import TvShow
 
 def test_new_show_name():
-    show = TvShow("Title")
-    assert show.name == "Title"
+    show = TvShow("my title")
+    assert show.name == "My Title"
 
 def test_new_show_have_empty_episodes_list():
     show = TvShow("Title")
