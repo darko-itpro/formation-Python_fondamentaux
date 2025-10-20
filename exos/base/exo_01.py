bc23bdac5f9b5f98d0e6164f04e80aab3aacba1c
Darko Stankovski <darko.itpro@gmail.com>
2025-10-20 14:04:34 +0200

    Premiers exercices

---
+++ exos/base/exo_01.py
@@ -0,0 +1,13 @@
+duration = 5
+daily_duration = 7
+
+print(duration)
+print(daily_duration)
+
+print(duration, daily_duration)
+
+print(duration * daily_duration)
+
+lunch_break = 73
+print(lunch_break // 60, lunch_break % 60)
+print(divmod(lunch_break, 60))
