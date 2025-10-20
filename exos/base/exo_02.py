92c2bc22d5fb3bf9ba8802418b7d42c90357c01c
Darko Stankovski <darko.itpro@gmail.com>
2025-10-20 15:41:09 +0200

    Ajout des listes et correction des exercices.

--- exos/base/exo_02.py
+++ exos/base/exo_02.py
@@ -1,2 +1,14 @@
 
 episode = ["The new Project", 1, 98, 2]
+
+print(episode[0], episode[2])
+
+title, _, duration, _ = episode
+
+print(title, duration)
+
+print(episode)
+episode[3] = episode[3]  + 1
+print(episode)
+episode[3] += 1
+print(episode)
--- exos/base/exo_03_01.py
+++ exos/base/exo_03_01.py
@@ -3,4 +3,9 @@ episode_viewed = ["The new Project", 1, 98, True]
 episode_not_viewed = ['Installing the softwares', 2, 42, False]
 
 episode = episode_viewed
-# episode = episode_not_viewed
+episode = episode_not_viewed
+
+if episode[3]:
+    print(' + Episode vu')
+else:
+    print(' - Episode Non vu')
