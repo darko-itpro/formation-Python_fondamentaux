62ad355956028d8d8bd6d19efb26a4375f8b1270
Darko Stankovski <darko.itpro@gmail.com>
2025-10-21 10:13:43 +0200

    Tous tests premier exercice

--- tests/exos/base/episode_utils/test_is_viewed.py
+++ tests/exos/base/episode_utils/test_is_viewed.py
@@ -7,3 +7,12 @@ def test_episode_viewed():
 def test_episode_not_viewed():
     episode = ['Installing the softwares', 2, 42, False]
     assert is_viewed(episode) is False
+
+def test_episode_viewed_as_count():
+    episode = ["The new Project", 1, 98, 2]
+    assert is_viewed(episode) is True
+
+def test_episode_not_viewed_as_count():
+    episode = ["The new Project", 1, 98, 0]
+    assert is_viewed(episode) is False
+
