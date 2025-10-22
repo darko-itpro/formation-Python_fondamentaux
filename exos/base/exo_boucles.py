9d74f88bf49716cf4b31f70dacc254e4d3df989f
Darko Stankovski <darko.itpro@gmail.com>
2025-10-22 10:46:02 +0200

    Fin exo boucles

--- exos/base/exo_boucles.py
+++ exos/base/exo_boucles.py
@@ -1,8 +1,18 @@
 import exos.base.episode_utils as eu
 import pyflix.datasource as ds
+from pprint import pprint
 
 bbt_s12 = ds.get_season('The Big Bang Theory', "Sheldon Cooper")
 
-playllist = eu.get_playlist_from(bbt_s12)
+playlist = eu.get_playlist_from(bbt_s12)
 
+time_left = 120
 
+while playlist and time_left > playlist[0]["duration"]:
+    episode = playlist.pop(0)
+    print(episode['title'])
+    time_left -= episode['duration']
+    episode['viewed'] = True
+
+print(time_left)
+pprint(bbt_s12)
