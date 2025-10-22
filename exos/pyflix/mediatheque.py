b107387cd35967c4cf38c3de741ba0b5734a8537
Darko Stankovski <darko.itpro@gmail.com>
2025-10-22 17:19:43 +0200

    Début modèle objet

--- exos/pyflix/mediatheque.py
+++ exos/pyflix/mediatheque.py
@@ -10,4 +10,11 @@ class Episode:
 
 
 class TvShow:
-    pass
+    def __init__(self, name:str):
+        self.name = name.title()
+        self.episodes = []
+
+    def add_episode(self, title:str, season_number:int, number:int,
+                    duration:int = None, year:int = None):
+        new_episode = Episode(title, season_number, number, duration, year)
+        self.episodes.append(new_episode)
---
+++ tests/exos/pyflix/test_tvshow.py
@@ -0,0 +1,26 @@
+import pytest
+from exos.pyflix.mediatheque import TvShow
+
+def test_new_show():
+    show = TvShow("Dr. who")
+    assert show.name == "Dr. Who"
+    assert show.episodes == []
+
+@pytest.fixture
+def show():
+    return TvShow("Dr. Who")
+
+def test_add_one_episode(show):
+    show.add_episode("Rose", 1, 1)
+    assert len(show.episodes) == 1
+    assert show.episodes[0].title == "Rose"
+
+def test_add_one_episode_with_duration(show):
+    show.add_episode("Rose", 1, 1, 54)
+    assert len(show.episodes) == 1
+    assert show.episodes[0].duration == 54
+
+def test_add_one_episode_with_year(show):
+    show.add_episode("Rose", 1, 1, 54, 2006)
+    assert len(show.episodes) == 1
+    assert show.episodes[0].year == 2006
