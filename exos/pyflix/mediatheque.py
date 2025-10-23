09099fdd52c6e6cfcfb173c448f2bd58ea03b14c
Darko Stankovski <darko.itpro@gmail.com>
2025-10-23 17:13:55 +0200

    Itérateur et méthode de classe.

--- exos/pyflix/mediatheque.py
+++ exos/pyflix/mediatheque.py
@@ -17,10 +17,38 @@ class Episode:
     def __str__(self):
         return f"{self.title} : s{self.season_number:02}e{self.number:02}"
 
+
+class EpisodesIterator:
+    def __init__(self, episodes:list[Episode]):
+        self._episodes = episodes.copy()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            return self._episodes.pop(0)
+        except IndexError:
+            raise StopIteration()
+
+
 class TvShow:
     def __init__(self, new_name:str):
         self.name = new_name
         self._episodes = []
+
+    @classmethod
+    def with_episodes(cls, name, episodes):
+        show = cls(name)
+        for episode_data in episodes:
+            show.add_episode(*episode_data)
+
+        return show
+
+
+    def __iter__(self):
+        return EpisodesIterator(self.episodes)
+        #return (episode for episode in self._episodes)
 
     @property
     def duration(self):
@@ -37,6 +65,12 @@ class TvShow:
     @property
     def episodes(self):
         return self._episodes.copy()
+
+    def __len__(self):
+        return len(self._episodes)
+
+    def __contains__(self, item):
+        return item in self._episodes
 
     def add_episode(self, title:str, season_number:int, number:int,
                     duration:int = None, year:int = None):
