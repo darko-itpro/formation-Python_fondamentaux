eaad72fccff665b89bc01b7502daf2ca288a8ae4
Darko Stankovski <darko.itpro@gmail.com>
2025-11-25 12:26:59 +0100

    Demo Import

---
+++ demos/demo_main.py
@@ -0,0 +1,38 @@
+episode_viewed = ["The new Project", 1, 98, True]
+episode_not_viewed = ['Installing the softwares', 2, 42, False]
+
+
+import demos.episode_util
+demos.episode_util.is_viewed(episode_viewed)
+
+from demos import episode_util
+episode_util.is_viewed(episode_not_viewed)
+
+import demos.episode_util as eu
+eu.is_viewed(episode_not_viewed)
+
+from demos.episode_util import is_viewed
+is_viewed(episode_not_viewed)
+
+print(type(eu))
+
+
+
+# network = get_network()
+#
+# if network:
+#     import ws_util as data_util
+# else:
+#     import cache_util as data_util
+#
+# data_util.save(data)
+
+
+
+
+
+
+
+
+
+
---
+++ demos/episode_util.py
@@ -0,0 +1,8 @@
+def is_viewed(episode:list):
+
+    viewed = episode[3]
+    if viewed:
+        print("Vu")
+    else:
+        print("pas vu")
+
