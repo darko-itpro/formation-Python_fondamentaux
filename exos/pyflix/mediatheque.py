2dbac77c9dfd5a3e5d2f54be8f17367b5c02c3e8
Darko Stankovski <darko.itpro@gmail.com>
2025-11-28 09:59:18 +0100

    Ajout méthodes spéciales

--- exos/pyflix/mediatheque.py
+++ exos/pyflix/mediatheque.py
@@ -8,6 +8,18 @@ class Episode:
         self.duration = duration
         self.year = year
 
+    def __str__(self):
+        return f'Episode "{self.title}", s{self.season_number:02}e{self.number:02}'
+
+    def __repr__(self):
+        return f'Episode({self.title}, {self.season_number}, {self.number}, {self.duration}, {self.year})'
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+
+        return (self.number, self.season_number) == (other.number, other.season_number)
+
 
 class TvShow:
     def __init__(self, title: str):
@@ -16,4 +28,19 @@ class TvShow:
 
     def add_episode(self, title: str, season_number: int, number: int, duration: int = None, year: int = None):
         new_episode = Episode(title, season_number, number, duration, year)
+        if new_episode in self.episodes:
+            raise ValueError('Episode already exists')
+
         self.episodes.append(new_episode)
+
+
+    def __str__(self):
+        return f'TvShow "{self.title}"'
+
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        # or: class_name = self.__class__.__name__
+        return f"{class_name}(name={self.name!r})"
+
+
