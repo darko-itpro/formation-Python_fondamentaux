4f88d65361b26ac0f176bb44f867a762d442c1a0
Darko Stankovski <darko.itpro@gmail.com>
2025-12-16 16:18:32 +0100

    Tests paramétrés

--- tests/exos/test_media_utils.py
+++ tests/exos/test_media_utils.py
@@ -1,9 +1,18 @@
+import pytest
 from exos.base.media_utils import is_viewed
 
-def test_episode_is_viewed_as_bool():
-    episode = {"title": "The Conjugal Configuration", "duration": 20, "viewed": True}
-    assert is_viewed(episode) is True
+test_data = [
+    ({"title": "The Conjugal Configuration", "duration": 20, "viewed": True}, True),
+    ({"title": "The Conjugal Configuration", "duration": 20, "viewed": False}, False),
+    ({"title": "The Conjugal Configuration", "duration": 20, "viewed": 5}, True),
+    ({"title": "The Conjugal Configuration", "duration": 20, "viewed": 3, "year": 2019}, True),
+    ({"title": "The Conjugal Configuration", "duration": 20, "viewed": 0}, False),
+    ({"title": "The Conjugal Configuration", "duration": 20}, False),
+    ({"title": "The Conjugal Configuration", "duration": 20, "year":2018}, False),
+]
 
-def test_episode_is_not_viewed_as_bool():
-    episode = {"title": "The Conjugal Configuration", "duration": 20, "viewed": False}
-    assert is_viewed(episode) is False
+
+@pytest.mark.parametrize("episode, expected", test_data)
+def test_episode_is_viewed_as_bool(episode, expected):
+    assert is_viewed(episode) is expected
+
