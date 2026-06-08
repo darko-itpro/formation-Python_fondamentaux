7fd4c2cfdcfb98ce0327f7e4fb97c90c928f9e87
Darko Stankovski <darko.itpro@gmail.com>
2026-06-08 17:22:25 +0200

    Exos jusqu'aux conditionnelles (sans la fin)

--- exos/base/exo_03_02.py
+++ exos/base/exo_03_02.py
@@ -4,3 +4,9 @@ episode_not_viewed = ['Installing the softwares', 2, 42, 0]
 
 episode = episode_viewed
 # episode = episode_not_viewed
+
+viewed = episode[3]
+if viewed > 0:
+    print("Episode vu")
+else:
+    print("Episode non vu")
