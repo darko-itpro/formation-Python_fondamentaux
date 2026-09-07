1d1544b7292b44127219cfd6184b509ec2b6ca43
Darko Stankovski <darko.itpro@gmail.com>
2026-09-07 23:24:39 +0200

    Ajout des deux premiers exercices

--- exos/exo_02.py
+++ exos/exo_02.py
@@ -1,2 +1,17 @@
 
 episode = ["The new Project", 1, 98, 2]
+
+print(episode[0], episode[2])
+
+title, _, duration, _ = episode
+
+print(title, duration)
+
+episode[3] = episode[3] + 1
+print(episode)
+episode[3] += 1
+print(episode)
+
+day = 48
+day %= 7
+print(day)
