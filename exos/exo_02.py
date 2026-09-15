0cb1796d31244fee3176a2748e4fadac7e61bc77
Darko Stankovski <darko.itpro@gmail.com>
2026-09-15 09:53:17 +0200

    Exo lists 1

--- exos/exo_02.py
+++ exos/exo_02.py
@@ -1,2 +1,24 @@
 
 episode = ["The new Project", 1, 98, 2]
+
+print(episode[0], episode[2])
+
+title, _, duration, _ = episode
+
+print(title, duration)
+
+print("----")
+
+print(episode[3])
+episode[3] = episode[3] + 1
+print(episode)
+episode[3] = episode[3] + 1
+print(episode)
+
+
+print("-- marche pas --")
+
+title, _, _, viewed = episode
+print(viewed, episode[3])
+viewed = viewed + 1
+print(viewed, episode[3])
